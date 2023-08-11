6ceee3e395a3d1b48a8b7666bc9ad8138e08282d
Zerlix2744 <monk27443@gmail.com>
2023-08-11 16:48:55 +0700

    create class-component and edit app.js
    create class-component.js and edit app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Header,Content,Footer } from './func-component'
+import Calendar from './class-component'
 
 function App(){
   return(
-    [<Header/>,<Content/>,<Footer/>]
+    <>
+      <Header/>
+      <p><center><Calendar/></center></p>
+      <Content/>
+      <Footer/>
+    </>
   )
 }
 export default App
