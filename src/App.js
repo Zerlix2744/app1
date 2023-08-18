d6739e55e9bc89bd9e997a4cf2ce7299b79f2ebf
Zerlix2744 <monk27443@gmail.com>
2023-08-18 15:38:02 +0700

    Banners.js
    Create banner.js & edit app.js
    Create banners folder in public

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react'
-import { Header,Content,Footer } from './func-component'
-import Calendar from './class-component'
+import Banner from './banner'
 
-function App(){
-  return(
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+export default function App(){
+  return<Banner/>
 }
-export default App
